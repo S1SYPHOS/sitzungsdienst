abde04e43b1d00636ce783b3b9443c7af8fbf2ae
Martin Folkers <hello@twobrain.io>
2021-10-12 13:44:54 +0200

    Bump version

--- sta.py
+++ sta.py
@@ -271,7 +271,7 @@ def dump_ics(data: list, ics_file: str) -> None:
 @click.option('-o', '--output-file', default='data', type=click.Path(), help='Output filename, without extension.')
 @click.option('-f', '--file-format', default='csv', help='File format, "csv", "json" or "ics".')
 @click.option('-q', '--query', multiple=True, help='Filter assignees, eg by name or department.')
-@click.version_option('1.0.0')
+@click.version_option('1.1.0')
 def cli(input_file, output_file, file_format, query):
     # If no input file provided ..
     if not input_file:
